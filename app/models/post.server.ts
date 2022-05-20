0e0ea48de5311488551d9d9b280bf6534a22716d
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-05-20 11:50:59 +0100

    Admin section to create posts

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,6 @@
 import { prisma } from "~/db.server";
 
-export type { Post } from "@prisma/client";
+import type { Post } from "@prisma/client";
 
 export async function getPosts() {
   return prisma.post.findMany();
@@ -9,3 +9,11 @@ export async function getPosts() {
 export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
 }
+
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
+  return prisma.post.create({ data: post });
+}
+
+export type { Post };
