9f976f8395987df6808dd40db46819f662b49df6
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-05-20 12:08:30 +0100

    Tweak UI of Uses

--- app/routes/uses/uses.tsx
+++ app/routes/uses/uses.tsx
@@ -3,15 +3,21 @@ import { UsesLoaderData } from ".";
 export default function Uses({ html }: UsesLoaderData) {
   return (
     <section>
-      <h1 className="my-4 text-4xl font-bold tracking-tight">Uses</h1>
-      <img
-        className="max-w-lg md:max-w-2xl xl:max-w-5xl"
-        alt="Uses"
-        src="https://res.cloudinary.com/dcrbjmmo4/image/upload/v1621104123/IMG_20210419_153903_4.jpg"
-      />
-      <article className="prose prose-blue prose-headings:font-light prose-h1:mt-8 prose-li:font-mono">
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </article>
+      <h1 className="my-4 flex justify-center text-4xl font-bold tracking-tight">
+        Uses
+      </h1>
+      <div className="flex flex-wrap justify-center">
+        <img
+          className="h-auto w-5/6 rounded-full xl:w-3/5"
+          alt="Uses"
+          src="https://res.cloudinary.com/dcrbjmmo4/image/upload/v1621104123/IMG_20210419_153903_4.jpg"
+        />
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <article className="prose prose-blue prose-headings:font-light prose-h1:mt-8 prose-li:font-mono">
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+        </article>
+      </div>
     </section>
   );
 }
